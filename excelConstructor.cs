1644709e9cf5b264f7afcc2b1aa8d37496b82611
jvoiharvey <jvoyharvey@rocketmail.com>
2022-07-25 02:53:12 -0500

    Create excelConstructor.cs

---
+++ excelConstructor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripsApp
+{
+    class excelConstructor
+    {
+        public const string excelConnection = @"";//insert excel file path
+       
+
+    }
+}
